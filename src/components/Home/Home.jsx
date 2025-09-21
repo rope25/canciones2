b0f03d329230a61ebadd6068c6ba496efda97ceb
rope25 <robertopelayo@gmail.com>
2025-09-21 16:55:53 +0200

    cancionescss

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,13 @@
-import styles from ".src/components/Home/Home.module.css";
+//import styles from ".src/components/Home/Home.module.css";
+import styles from './Home.module.css';
+
 
 export default function Home() {
     return (
 
-        <div className={styles.container}>
-        
-            <img src="./public/dulce introduccion al caos.jpg" alt="imagen portada" />
-            
-                    <h2 class="texto bienvenida" styles="bold" >Bienvenido a la app de canciones</h2>
- 
-</div>
+       <div className={styles.container}>Bienvenido a la app de canciones</div>
 
         
-    );
+    )
 }
 
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate, NavLink, Outlet } from "react-router-dom";
+import Home from "../components/Home/Home";
 import ListaCancionesPreferidas from "../components/ListaCancionesPreferidas/ListaCancionesPreferidas";
 import Analitica from "../components/Analitica/Analitica";
 import ContactForm from "../components/ContactForm/ContactForm";
-import Home from "../components/Home/Home";
 import SobreMi from "../components/SobreMi/SobreMi";
 
 function Layout() {
@@ -76,7 +76,7 @@ function NotFound() {
   return (
     <>
       <p className="error404">404 · Ruta no encontrada</p>
-      <img src="/error404.png" alt="Not Found" className="error404-image" />
+      <img src=".public/404.png" alt="Not Found" className="error404-image" />
     </>
   );
 }
@@ -89,8 +89,8 @@ export default function AppRouter() {
         {/* index: cuando visites exactamente "/" redirige a /home */}
         <Route index element={<Navigate to="/Home" replace />} />
         {/* Rutas hijas */}
-        <Route path="/Home" element={<Home />} /> 
-        <Route path="/ListadeCancionesPreferidas" element={<ListaCancionesPreferidas />} />
+        {/* <Route path="/Home" element={<Home />} />  */}
+        <Route path="/ListaCancionesPreferidas" element={<ListaCancionesPreferidas />} />
         <Route path="/Analitica" element={<Analitica />} />
         <Route path="/ContactForm" element={<ContactForm />} />
         <Route path="/SobreMi" element={<SobreMi />} /> 
